b75519ed643cfe95b5a5b59480d6299cc517f762
Lubos Kolumber <lubosk158@gmail.com>
2023-12-02 17:05:58 +0100

    process important

--- app/api/send/route.tsx
+++ app/api/send/route.tsx
@@ -2,7 +2,7 @@ import { generateEmailHtml } from "@/app/components/generateEmailHtml";
 import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY!);
 
 export async function POST(req: NextRequest) {
   const { email, images } = await req.json();
